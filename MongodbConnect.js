262aa273763f173222d940373936385bebc70bfd
Aryan <Aryan1284@gmail.com>
2025-05-20 20:23:30 +0530

    new

--- MongodbConnect.js
+++ MongodbConnect.js
@@ -1,11 +1,13 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
-    console.log('MongoDB connected');
+    await mongoose.connect(`${process.env.MONGO_URI}/eventScrapper`)
+    console.log("MongoDB connected");
   } catch (err) {
-    console.error('Error connecting to MongoDB:', err);
+    console.error("MongoDB connection failed", err);
     process.exit(1);
   }
 };
+
+module.exports = connectDB;
